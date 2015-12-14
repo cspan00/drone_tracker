e6b4af0a38ce3f61421e158a7752aab10c75acee
Corey Yaspan <cspan00@gmail.com>
2015-12-14 15:12:56 -0700

    added header

--- functions.js
+++ functions.js
@@ -19,21 +19,28 @@ function getLon(){
 
 
 
+// idea!!! to select year, make a new function for each year that only populates the map by selected year.
+
+
+
+
 function createMap(){
 var mapOptions = {
     center: new google.maps.LatLng(20.33877,57.43193),
     zoom: 5,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeId: google.maps.MapTypeId.HYBRID
 };
 var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
+// iterate through the arrays returned by getLat and GetLon and place markers on the map
 for( i = 0; i < getLon().length; i++ ) {
         var position = new google.maps.LatLng(getLat()[i], getLon()[i]);
-        // bounds.extend(position);
+
         marker = new google.maps.Marker({
+            // html: infoHtml,
             position: position,
             map: map,
-            // title: markers[i][0]
+
         });
 }
 };
